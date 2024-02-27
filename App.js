cf9a3cadb3730850a06290f1add49ea8aa8f1ff4
Strange <rastogi1225@gmail.com>
2024-02-28 01:24:20 +0530

    Configured the parcel into my app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div",{},[
 React.createElement("ul",{key:"list1"},[
     React.createElement("li",{key:"1"},"Unordered list item -1"),
@@ -12,6 +14,7 @@ React.createElement("ol",{key:"list2"},[
     React.createElement("li",{key:"2"},"Ordered list item -2")
 ])
 ]);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
