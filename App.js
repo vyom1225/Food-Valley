34b390333daec4df975ac2dd969ceb53b6a7f222
Strange <rastogi1225@gmail.com>
2024-02-28 01:56:40 +0530

    Started using jsx

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("div",{},[
-React.createElement("ul",{key:"list1"},[
-    React.createElement("li",{key:"1"},"Unordered list item -1"),
-    React.createElement("li",{key:"2",
-        style:{
-            color:"pink",
-            backgroundColor:"green",
-        }},"Unordered list item -2")
-]),
-React.createElement("ol",{key:"list2"},[
-    React.createElement("li",{key:"1"},"Ordered list item -1"),
-    React.createElement("li",{key:"2"},"Ordered list item -2")
-])
-]);
+
+const Heading = ()=>{
+    return(
+    <h1>Hello World from jsx</h1>
+    )
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading/>);
