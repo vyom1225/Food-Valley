7de1e3b98d11645b1de3c08dacfd63dbe42f01a2
Strange <rastogi1225@gmail.com>
2024-02-27 22:14:45 +0530

    Inception

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("div",{},[
+React.createElement("ul",{key:"list1"},[
+    React.createElement("li",{key:"1"},"Unordered list item -1"),
+    React.createElement("li",{key:"2",
+        style:{
+            color:"pink",
+            backgroundColor:"green",
+        }},"Unordered list item -2")
+]),
+React.createElement("ol",{key:"list2"},[
+    React.createElement("li",{key:"1"},"Ordered list item -1"),
+    React.createElement("li",{key:"2"},"Ordered list item -2")
+])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
