41b5e0565415cbcc702d8ec43d11869a1128db2b
Strange <rastogi1225@gmail.com>
2024-04-30 22:56:53 +0530

    just testing

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./src/components/Header.js"
-import Body from "./src/components/Body.js"
+import Header from "./src/components/Header.js";
+import Body from "./src/components/Body.js";
 
 const AppLayout = () => (
         <>
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,20 @@
 import RestaurantCard from "./RestaurantCard.js"
 import resData from "../utils/resData.js"
 import {useState} from "react";
+
+let filterBtnName = "Top Rated Restaurants";
 const Body = ()=>{
-    const [filterBtnName,setfilterBtnName] = useState("Top Rated Restaurants");
     const [listOfRestaurants,setlistOfRestaurants] = useState(resData);
     return (<div className = "body">
     <div className = "filter-btn">
         <button onClick={()=>{
             if(listOfRestaurants===resData){
-                setfilterBtnName("All Restaurants");
+                filterBtnName ="All Restaurants"
                 setlistOfRestaurants(listOfRestaurants.filter((res)=>{
                     return res.info.avgRating >=4.5;
                 }));
             }else{
-                setfilterBtnName("Top Rated Restaurants");
+                filterBtnName = "Top Rated Restaurants";
                 setlistOfRestaurants(resData);
             }
         }    
